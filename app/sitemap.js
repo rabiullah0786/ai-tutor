1aa2e0dced205c4ff23b59ae8aeb8baf790de1ec
hello rabiullah <rabiullahansari@hellos-MacBook-Pro.local>
2025-12-10 23:15:36 +0530

    SEO add

---
+++ app/sitemap.js
@@ -0,0 +1,11 @@
+export default function sitemap() {
+    return [
+      {
+        url: "https://televora.in",
+        lastModified: new Date(),
+        changeFrequency: "weekly",
+        priority: 1.0,
+      },
+    ];
+  }
+  

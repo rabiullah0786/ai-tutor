85c69bd6d88680de7746d847b6e84351a09b9bdd
hello rabiullah <rabiullahansari@hellos-MacBook-Pro.local>
2025-12-15 00:18:03 +0530

    some chenge

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,8 @@
 import Provider from "./provider";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
+
 
 
 const inter = Inter({ subsets: ["latin"] });
